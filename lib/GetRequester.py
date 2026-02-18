f1d41beb043d06e7b99d5387fc8d50569c8a4663
Michelle Di Guglielmo <mfdigug@gmail.com>
2026-02-19 09:13:13 +1100

    done with getting remote data lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,23 @@
 import requests
 import json
 
+
 class GetRequester:
 
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        url_data = []
+        data = json.loads(self.get_response_body())
+        for item in data:
+            new_item = {
+                "name": item["name"],
+                "occupation": item["occupation"]
+            }
+            url_data.append(new_item)
+        return url_data
